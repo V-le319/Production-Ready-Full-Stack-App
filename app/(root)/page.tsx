70051e46ffcdca51c965ffd9c8c17c0f06995389
V-le319 <200332747+V-le319@users.noreply.github.com>
2026-04-19 14:30:35 +0700

    install shadcn + apply search logic with async await

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import SearchForm from '../components/SearchForm'
 
-const home = () => {
+export default async function home ({searchParams} : {searchParams: Promise<{ query?: string }>} )
+ {
+
+  /*this is the URL part */
+    const query = (await searchParams).query;
+
   return (
     <>
-    <h1 className="heading">HOME</h1>
+    <section className="main_container">
+      <h1 className="heading">Pitch Your Startup, <br/> Connect With Entrepreneurs</h1>
+      <p className="sub-heading !max-text-3xl">Submit Ideas, Vote On Pitches, And Get Noticed in Virtual Competition.</p>
+        <SearchForm query={query}/>
+    </section>
+    
     </>
   )
 }
 
-export default home
+
---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,32 @@
+import  Form from "next/form"
+import React from 'react'
+import SearchFormReset from "./SearchFormReset";
+import { Search } from "lucide-react"
+
+const SearchForm = ({ query } : { query?: string }) => {
+    
+
+    
+  return (
+    <Form action="/" scroll={false}
+            className="search-form">
+
+            <input defaultValue={query}
+                    name="query"
+                    className="search-input"
+                    placeholder="Search Startups"
+            />
+
+            <div className="flex gap-2">
+                {query && <SearchFormReset/>}
+                <button type="submit"
+                        className="search-btn text-white"
+                >
+                        <Search/>
+                </button>
+            </div>
+    </Form>
+  )
+}
+
+export default SearchForm
