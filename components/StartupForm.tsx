5a331c290834e55086e2bd4eff555ad3a39d171a
V-le319 <200332747+V-le319@users.noreply.github.com>
2026-05-06 21:24:19 +0700

    building startup-form with MDEditor

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -2,17 +2,21 @@
 
 import { useState } from "react"
 import { Input } from "./ui/input"
+import { Textarea } from "./ui/textarea"
+import MDEditor from '@uiw/react-md-editor'
 
 
 
 
 const StartupForm = () => {
-    const [errors, setErrors] = useState<Record<string, string>>({})
+    const [errors, setErrors] = useState<Record<string, string>>({});
     /*Record<string, string> means "an object where both keys and values are strings" */
-    
+
     {/*You could write { title: string, description: string, category: string } manually
      but Record<string, string> is shorter and works for any field name without listing them all upfront. */}
   
+        const [pitch, setPitch] = useState("**Hello World**");
+
      return (
     <form className="startup-form"
             action={() => {}}>
@@ -26,7 +30,70 @@ const StartupForm = () => {
                     className="startup-form_input" required
                     placeholder="Startup Title"/>
             
-            {errors && <p className="startup-form_error">{errors.title}</p>}
+            {errors.title && <p className="startup-form_error">{errors.title}</p>}
+        </div>
+
+        
+        <div>
+            <label htmlFor="description"
+            className="startup-form_label">
+                Description
+            </label>
+            <Textarea id="description" name="description"
+                    className="startup-form_textarea" required
+                    placeholder="Startup Description"/>
+            
+            {errors.description && <p className="startup-form_error">{errors.description}</p>}
+        </div>
+
+        
+        <div>
+            <label htmlFor="category"
+            className="startup-form_label">
+                Category
+            </label>
+            <Input id="category" name="category"
+                    className="startup-form_input" required
+                    placeholder="Startup Category (Tech, health, Education, ..."/>
+            
+            {errors.category && <p className="startup-form_error">{errors.category}</p>}
+        </div>
+
+        
+        <div>
+            <label htmlFor="link"
+            className="startup-form_label">
+                Image URL
+            </label>
+            <Input id="link" name="link"
+                    className="startup-form_input" required
+                    placeholder="Startup Image URL"/>
+            
+            {errors.link && <p className="startup-form_error">{errors.link}</p>}
+        </div>
+
+        <div data-color-mode="light">
+            <label htmlFor="pitch"
+            className="startup-form_label">
+                Pitch
+            </label>
+          <MDEditor value={pitch}
+                    onChange={(value) => setPitch(value as string)}
+                    id="pitch"
+                    preview="edite"
+                    height={300}
+                    style={{overflow: "hidden", borderRadius: 20}}
+                    textareaProps={
+                        {placeholder: "Briefly describe your idea and what problem it solves."}
+                    }
+                    previewOptions={{
+                        
+                    }}
+                    >
+
+                    </MDEditor>
+            
+            {errors.pitch && <p className="startup-form_error">{errors.pitch}</p>}
         </div>
 
     </form>
