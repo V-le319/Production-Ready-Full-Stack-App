fc08d1dab268193789c160d55c30e06b84b76580
V-le319 <200332747+V-le319@users.noreply.github.com>
2026-05-14 14:14:45 +0700

    adding UI shadcn for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
 import {auth, signOut, signIn} from "@/auth"
-import { redirect } from 'next/dist/server/api-utils';
+//import { redirect } from 'next/dist/server/api-utils';
+import { BadgePlus, LogOut } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 
 const Navbar = async () => { {/*we're able to do this aync function here bcz we are on server side component */}
@@ -19,18 +21,25 @@ const Navbar = async () => { {/*we're able to do this aync function here bcz we 
           {session && session?.user ? (
             <>
           <Link href="/startup/create">
-          <span>Create</span>
+          <span className="max-sm:hidden">Create</span>
+          <BadgePlus className=" size-6 sm:hidden"/>
           </Link>
 
           <form action={async () => {
             "use server";
             await signOut({ redirectTo: "/" });
           }}>
-            <button type="submit">Logout</button>
+            <button type="submit">
+              <span className="max-sm:hidden">Logout</span>
+              <LogOut className=" size-6 sm:hidden text-red-600"/>
+            </button>
           </form>
 
             <Link href={`/user/${session?.id}`}>
-            <span>{session?.user?.name}</span>
+            <Avatar className="size-10">
+            <AvatarImage src={session?.user?.image} alt={session?.user?.name || ""}  />
+            <AvatarFallback>AV</AvatarFallback>
+            </Avatar>
             </Link>
           </>
           ) : (
