d2e027ae5c3acfed42ace00297ffcb9467e95f2f
V-le319 <200332747+V-le319@users.noreply.github.com>
2026-05-15 13:35:57 +0700

    project complete

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -88,3 +88,28 @@ export const STARTUP_VIEWS_QUERY= defineQuery(`*[_type == "startup" && _id == $i
   category, 
   immage
  }`);
+
+ export const PLAYLIST_BY_SLUG_QUERY =
+  defineQuery(`*[_type == "playlist" && slug.current == $slug][0]{
+  _id,
+  title,
+  slug,
+  select[]->{
+    _id,
+    _createdAt,
+    title,
+    slug,
+    author->{
+      _id,
+      name,
+      slug,
+      image,
+      bio
+    },
+    views,
+    description,
+    category,
+    immage,
+    pitch
+  }
+}`);
