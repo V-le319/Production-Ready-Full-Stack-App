70e93e8c16384305dd7d7b8030c693a2686d0eb5
V-le319 <200332747+V-le319@users.noreply.github.com>
2026-05-17 20:58:24 +0700

    redeploy

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -7,13 +7,24 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+// import { NextStudio } from 'next-sanity/studio'
+// import config from '../../../sanity.config'
+
+// export const dynamic = 'force-dynamic'
+
+// export { metadata, viewport } from 'next-sanity/studio'
+
+// export default function StudioPage() {
+//   return <NextStudio config={config} />
+// }
 
 export const dynamic = 'force-dynamic'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
-export default function StudioPage() {
+export default async function StudioPage() {
+  const { NextStudio } = await import('next-sanity/studio')
+  const config = (await import('../../../sanity.config')).default
+  
   return <NextStudio config={config} />
 }
