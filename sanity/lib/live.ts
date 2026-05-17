11e61a82aad394751f0d3ff6d6ef25834228371e
V-le319 <200332747+V-le319@users.noreply.github.com>
2026-05-17 21:12:26 +0700

    adding SANITY_WRITE_TOKEN to live.ts

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -6,5 +6,7 @@ import { defineLive } from "next-sanity/live";
 import { client } from './client';
 
 export const { sanityFetch, SanityLive } = defineLive({
-  client,
+  client: client.withConfig({
+     token: process.env.SANITY_WRITE_TOKEN,
+  }),
 });
